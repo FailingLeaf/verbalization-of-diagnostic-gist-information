fe5e36fd4b0fa16510f438b28bcbc80f80bf7ba1
Failing Leaf <niklas.lewalter@gmail.com>
2026-01-22 13:12:54 +0100

    stim list for pilot

---
+++ smol_stimnames.js
@@ -0,0 +1,29 @@
+var stimuli_0 = [
+{image: "stims/beach_lay.png", prestime: "250", condition: "lay", mask: "masks/mask(1).jpg", data: {file: "beach_lay.png", prestime: "250", condition: "lay", eventtype:"response"}},
+{image: "stims/flug_lay.png", prestime: "50", condition: "lay", mask: "masks/mask(2).jpg", data: {file: "flug_lay.png", prestime: "250", condition: "lay", eventtype:"response"}},
+{image: "stims/skate_obj.png", prestime: "150", condition: "obj", mask: "masks/mask(3).jpg", data: {file: "skate_obj.png", prestime: "250", condition: "obj", eventtype:"response"}},
+{image: "stims/play_obj.png", prestime: "250", condition: "obj", mask: "masks/mask(4).jpg", data: {file: "play_obj.png", prestime: "250", condition: "obj", eventtype:"response"}},
+{image: "stims/bath_sc.png", prestime: "50", condition: "sc", mask: "masks/mask(5).jpg", data: {file: "bath_sc.png", prestime: "250", condition: "sc", eventtype:"response"}},
+];
+
+var stimuli_1 = [
+{image: "stims/beach_obj.png", prestime: "250", condition: "obj", mask: "masks/mask(1).jpg", data: {file: "beach_obj.png", prestime: "250", condition: "obj", eventtype:"response"}},
+{image: "stims/flug_obj.png", prestime: "50", condition: "obj", mask: "masks/mask(2).jpg", data: {file: "flug_obj.png", prestime: "250", condition: "obj", eventtype:"response"}},
+{image: "stims/skate_sc.png", prestime: "150", condition: "sc", mask: "masks/mask(3).jpg", data: {file: "skate_sc.png", prestime: "250", condition: "sc", eventtype:"response"}},
+{image: "stims/play_sc.png", prestime: "250", condition: "sc", mask: "masks/mask(4).jpg", data: {file: "play_sc.png", prestime: "250", condition: "sc", eventtype:"response"}},
+{image: "stims/bath_lay.png", prestime: "50", condition: "lay", mask: "masks/mask(5).jpg", data: {file: "bath_lay.png", prestime: "250", condition: "lay", eventtype:"response"}},
+];
+
+var stimuli_2 = [
+{image: "stims/beach_sc.png", prestime: "250", condition: "sc", mask: "masks/mask(1).jpg", data: {file: "beach_sc.png", prestime: "250", condition: "sc", eventtype:"response"}},
+{image: "stims/flug_sc.png", prestime: "50", condition: "sc", mask: "masks/mask(2).jpg", data: {file: "flug_sc.png", prestime: "250", condition: "sc", eventtype:"response"}},
+{image: "stims/skate_lay.png", prestime: "150", condition: "lay", mask: "masks/mask(3).jpg", data: {file: "skate_lay.png", prestime: "250", condition: "lay", eventtype:"response"}},
+{image: "stims/play_lay.png", prestime: "250", condition: "lay", mask: "masks/mask(4).jpg", data: {file: "play_lay.png", prestime: "250", condition: "lay", eventtype:"response"}},
+{image: "stims/bath_obj.png", prestime: "50", condition: "obj", mask: "masks/mask(5).jpg", data: {file: "bath_obj.png", prestime: "250", condition: "obj", eventtype:"response"}},
+];
+
+var practice_stimuli = [
+{image: "stims/bath_prac1.png", prestime: "250", condition: "prac", mask: "masks/mask(1).jpg", data: {file: "bath_prac1.png", prestime: "250", condition: "prac", eventtype:"response"}},
+{image: "stims/bath_prac2.png", prestime: "250", condition: "prac", mask: "masks/mask(2).jpg", data: {file: "bath_prac2.png", prestime: "250", condition: "prac", eventtype:"response"}},
+{image: "stims/bath_prac3.png", prestime: "250", condition: "prac", mask: "masks/mask(3).jpg", data: {file: "bath_prac3.png", prestime: "250", condition: "prac", eventtype:"response"}}
+];
